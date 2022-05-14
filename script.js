51a4ab9fec1b046e5a2d3c7de8b72c6b0805a2cd
Jess Hause Ugray <jess.ugray@gmail.com>
2022-05-14 07:32:47 -0400

    js updated to include front end api call based on form input

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-require('dotenv').config();
 
-const apiSecret = process.env.OPENAI_API_KEY;
+const apiSecret = OPENAI_API_KEY;
+const promptButton = document.getElementById("submit");
 
-console.log(process.env);
 console.log(apiSecret);
+
+function apiCall2() {
+  event.preventDefault();
+  console.log("hello")
+}
+
+function apiCall() {
+  event.preventDefault();
+  console.log("api call function ran")
+  const input = document.getElementById("prompt").value.trim();
+  console.log(input);
+  const data = {
+    prompt: input,
+    temperature: 0.5,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+   };
+    
+   fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiSecret}`,
+    },
+    body: JSON.stringify(data),
+   })
+   .then(function (response) {
+     if (response.ok) {
+       response.json().then(function(data) {
+         console.log(data);
+       })
+     }
+   })
+
+}
+
+promptButton.addEventListener("click", apiCall);
