2395fb3cfe2bbc63eed242bd2f4c35617701a7a5
Jess Hause Ugray <jess.ugray@gmail.com>
2022-05-16 17:48:28 -0400

    font colouring updated to white where background is dark, twitter share button added, regex used to remove new lines from api response

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ console.log(apiSecret);
 function apiCall() {
   event.preventDefault();
   console.log("api call function ran")
-  const input = document.getElementById("prompt").value.trim();
+  const textArea = document.getElementById("prompt");
+  const input = textArea.value.trim();
+  textArea.value = "";
   console.log(input);
   const data = {
+    //prompt contains instruction plus user input
     prompt: "Convert this into emojis\n\n" + input + ".",
     temperature: 0.1,
     max_tokens: 50,
@@ -39,16 +42,27 @@ function apiCall() {
         responseHeader.innerText = "✨Responses✨";
         const resultContainer = document.createElement("div");
         resultContainer.className = "container has-background-primary mt-6 px-3 rounded-corners";
+        //prepend instead of appendChild to results go from newest to oldest
         parent.prepend(resultContainer);
         const responsesPrompt = document.createElement("div");
         const responsesResponse = document.createElement("div");
-        responsesPrompt.className = "columns is-mobile";
-        responsesResponse.className = "columns is-mobile";
+        responsesPrompt.className = "columns is-mobile has-text-white";
+        responsesResponse.className = "columns is-mobile has-text-white";
         resultContainer.appendChild(responsesPrompt);
         resultContainer.appendChild(responsesResponse);
         console.log(results);
-        responsesPrompt.innerHTML = "<h3 class='subtitle column is-narrow'>Prompt:</h3> " + "<p class='column'>" + results[0].prompt + "</p><br />";
-        responsesResponse.innerHTML = "<h3 class='subtitle column is-narrow'>Response:</h3> " + "<p class='column'>" + results[0].response + "</p><br />"; 
+        const resultsPrompt = results[0].prompt;
+        //regex w/ global match used to remove new lines/breaks and insert a space instead
+        const resultsResponse = results[0].response.replace(/(\r\n|\n|\r)/g, " ");
+        responsesPrompt.innerHTML = "<h3 class='subtitle column is-narrow has-text-white'>Prompt:</h3> " + "<p class='column'>" + resultsPrompt + "</p><br />";
+        responsesResponse.innerHTML = "<h3 class='subtitle column is-narrow has-text-white'>Response:</h3> " + "<p class='column'>" + resultsResponse + "</p><br />"; 
+        const twitterShare = document.createElement("span");
+        twitterShare.className = "is-pulled-right pt-3 px-3 m-3 has-text-white has-background-info rounded-corners";
+        const tweet = encodeURI(resultsResponse);
+        console.log(tweet);
+        twitterShare.innerHTML = "<i class='fa-brands fa-twitter-square fa-xl'></i> <a class='has-text-white' target='blank' href='https://twitter.com/intent/tweet?text=" + tweet + "'>" + "Share on Twitter" + "</a>";
+        responsesResponse.appendChild(twitterShare);
+
        });
      };
    });
